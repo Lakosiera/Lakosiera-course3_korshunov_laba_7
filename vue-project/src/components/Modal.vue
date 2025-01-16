1371f18845164d9a22be28529bb2cd0cbe4b7f95
Lakosiera <lakosiera@mail.ru>
2025-01-16 16:41:45 +0500

    фронтэнд

---
+++ vue-project/src/components/Modal.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref, reactive, watch, onMounted } from 'vue'
+
+const props = defineProps({
+    id: String,
+    title: String,
+    show: Boolean
+})
+const emit = defineEmits(['onClose'])
+
+const elmRef = ref(null)
+const modalBS = ref(null)
+
+onMounted(() => {
+    modalBS.value = new bootstrap.Modal(elmRef.value)
+
+    elmRef.value.addEventListener('hidden.bs.modal', event => {
+        onClose()
+    })
+
+    if (props.show) {
+        onShow()
+    }
+})
+
+watch(
+  () => props.show,
+  (newValue) => {
+    if (newValue) {
+        onShow()
+    }
+  }
+)
+
+function onClose() {
+    emit('onClose')
+}
+
+function onShow() {
+    modalBS.value.show()
+}
+</script>
+
+<template>
+<div class="modal" tabindex="-1" ref="elmRef">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content _border shadow-lg">
+      <div class="modal-header">
+        <h5 class="modal-title">{{ title || '' }}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
